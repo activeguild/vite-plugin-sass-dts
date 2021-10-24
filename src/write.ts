23543a4cef89b8856837f89fbaaef17e7c960bb2
j1ngzoue <workdrive0306@gmail.com>
2021-10-25 02:34:35 +0900

    Add eslint and prettier

--- src/write.ts
+++ src/write.ts
@@ -1,44 +1,44 @@
-import fs from "fs";
-import path from "path";
-import { getRelativePath } from "./util";
+import fs from 'fs'
+import path from 'path'
+import { getRelativePath } from './util'
 
 export const writeToFile = (
   fileName: string,
   classNameKeys: Map<string, boolean>,
   globalOutFile?: string
 ) => {
-  let exportTypes = "";
-  let exportStyle = "export = classNames;";
+  let exportTypes = ''
+  const exportStyle = 'export = classNames;'
   for (const classNameKey of classNameKeys.keys()) {
-    exportTypes = `${exportTypes}\n${formatExportType(classNameKey)}`;
+    exportTypes = `${exportTypes}\n${formatExportType(classNameKey)}`
   }
 
-  let outputFileString = "";
+  let outputFileString = ''
   if (globalOutFile) {
     const relativePath = getRelativePath(
       path.dirname(fileName),
       path.dirname(globalOutFile)
-    );
-    const exportTypeFileName = formatExportTypeFileName(globalOutFile);
-    outputFileString = `import globalClassNames from '${relativePath}${exportTypeFileName}'\n`;
-    outputFileString = `${outputFileString}declare const classNames: typeof globalClassNames & {${exportTypes}\n};\n${exportStyle}`;
+    )
+    const exportTypeFileName = formatExportTypeFileName(globalOutFile)
+    outputFileString = `import globalClassNames from '${relativePath}${exportTypeFileName}'\n`
+    outputFileString = `${outputFileString}declare const classNames: typeof globalClassNames & {${exportTypes}\n};\n${exportStyle}`
   } else {
-    outputFileString = `declare const classNames: {${exportTypes}\n};\n${exportStyle}`;
+    outputFileString = `declare const classNames: {${exportTypes}\n};\n${exportStyle}`
   }
 
   fs.writeFile(formatWriteFileName(fileName), `${outputFileString}`, (err) => {
     if (err) {
-      console.log(err);
-      throw err;
+      console.log(err)
+      throw err
     }
-  });
-};
+  })
+}
 
 export const formatExportType = (key: string) =>
-  `  readonly '${key}': '${key}';`;
+  `  readonly '${key}': '${key}';`
 
 export const formatWriteFileName = (file: string) =>
-  `${file}${file.endsWith("d.ts") ? "" : ".d.ts"}`;
+  `${file}${file.endsWith('d.ts') ? '' : '.d.ts'}`
 
 export const formatExportTypeFileName = (file: string) =>
-  path.basename(file.replace(".ts", ""));
+  path.basename(file.replace('.ts', ''))
