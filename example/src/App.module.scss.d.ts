0a012bac7492fb961103182f0e0ba0d17124ab90
j1ngzoue <workdrive0306@gmail.com>
2021-10-26 02:26:17 +0900

    Fix prettier

--- example/src/App.module.scss.d.ts
+++ example/src/App.module.scss.d.ts
@@ -1,6 +1,7 @@
-import globalClassNames from './style.d'
+import globalClassNames, { ClassNames as GlobalClassNames } from './style.d'
 declare const classNames: typeof globalClassNames & {
   readonly 'header-1': 'header-1'
   readonly active: 'active'
 }
-export = classNames
+export default classNames
+export type ClassNames = 'header-1' | 'active' | GlobalClassNames
